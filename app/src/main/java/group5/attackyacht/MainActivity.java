7306918b335a4cb86e81ae1ddd48aa26dfe72036
lostwithinadream <lostwithinadream@gmail.com>
2015-11-17 09:46:03 -0600

    Initial commit

---
+++ app/src/main/java/group5/attackyacht/MainActivity.java
@@ -0,0 +1,42 @@
+package group5.attackyacht;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+//        Button buttonTwoPlayer = (Button) findViewById(R.id.button_menu_twoPlayer);
+//        buttonTwoPlayer.setOnClickListener(new View.OnClickListener() {
+//            public void onClick(View v) {
+//                // Perform action on click
+//            }
+//        });
+//
+//        Button buttonOptions = (Button) findViewById(R.id.button_menu_options);
+//        buttonOptions.setOnClickListener(new View.OnClickListener() {
+//            public void onClick(View v) {
+//                // Perform action on click
+//            }
+//        });
+    }
+
+    public void onClickTwoPlayer(View v){
+
+
+    }
+
+    public void onClickOptions(View v){
+
+
+    }
+
+
+
+
+}
