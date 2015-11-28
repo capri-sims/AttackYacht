71b911249d5de68be0f4ae6f4fa04ac2a3599f9f
TrulyWilliams <tw013@my.tamuct.edu>
2015-11-28 16:04:29 -0600

    Program with buttons

--- app/src/main/java/group5/attackyacht/GameOverActivity.java
+++ app/src/main/java/group5/attackyacht/GameOverActivity.java
@@ -1,7 +1,10 @@
 package group5.attackyacht;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class GameOverActivity extends AppCompatActivity {
 
@@ -10,6 +13,31 @@ public class GameOverActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_over);
 
-        //three buttons
+        // button to return to main menu.
+        Button buttonMenu = (Button)findViewById(R.id.button_menu);
+        buttonMenu.setOnClickListener((v) -> {
+            Intent intent = new Intent(GameOverActivity.this,MainActivity.class);
+            startActivity(intent);
+        });
+
+        // button to play again
+        Button buttonAgain = (Button) findViewById(R.id.button_again);
+        buttonAgain.setOnClickListener((v) -> {
+            Intent intent = new Intent(GameOverActivity.this,SetupActivity.class);
+            startActivity(intent);
+        });
+
+        // button to exit
+        Button buttonExit = (Button)findViewById(R.id.button_exit);
+        buttonExit.setOnClickListener((v)-> {
+            @Override
+                    public void onClick(View v){
+                    finish();
+                    System.exit(0);
+            }
+        });
+
+
     }
 }
+
